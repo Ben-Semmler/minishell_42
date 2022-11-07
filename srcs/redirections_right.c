d0122fd410db97696c76b2adcbc6f702481d2c57
jaymie gobbett <jjjj7350@gmail.com>
2022-11-07 22:27:08 +1030

    fixed the right append not overwriting file

--- srcs/redirections_right.c
+++ srcs/redirections_right.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirections_right.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgobbett <jgobbett@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 12:21:09 by jgobbett          #+#    #+#             */
-/*   Updated: 2022/07/05 14:55:34 by jgobbett         ###   ########.fr       */
+/*   Updated: 2022/11/07 22:25:55 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	writeToFile(char *input, char *file)
 	//DEBUG
 
 	//Doesnt overwrite file
-	write(open(file, O_WRONLY | O_CREAT, 0644), input, ft_strlen(input));
+	write(open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644), input, ft_strlen(input));
 }
 
 void	writeToFile_append(char *input, char *file)
