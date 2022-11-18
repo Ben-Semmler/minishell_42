cda3217cb7bc15c3240ff152df7d85e164eaac6a
Jaymie Gobbett - jgobbett <87624652+Duggopriest@users.noreply.github.com>
2022-11-18 13:58:10 +1030

    Update minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jgobbett <jgobbett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 17:22:03 by bsemmler          #+#    #+#             */
-/*   Updated: 2022/11/14 16:01:51 by jgobbett         ###   ########.fr       */
+/*   Updated: 2022/11/18 13:57:57 by jgobbett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,32 +33,36 @@
 # include <sys/ioctl.h>  
 # include <fcntl.h>
 
-typedef struct s_inputs
+typedef struct s_container
+{
+	int		i1;
+	int		i2;
+	int		i3;
+	int		i4;
+	char	c1;
+	char	c2;
+	char	c3;
+	char	*cp1;
+	bool	b;
+}	t_container;
+
+
+typedef struct s_args
 {
 	int		argc;
 	char	**argv;
-	char	*stdin;
-}	t_inputs;
-
-typedef struct s_outputs
-{
-	char	*stdout;
-	char	*stderr;
-	int		returnval;
-}	t_outputs;
+}	t_args;
 
 typedef struct s_action
 {
 	char			*command;
-	int				argc;
-	char			**argv;
+	t_args			args;
 	char			*relation;
 	bool			fork;
 	struct s_action	*next;
 }	t_action;
 
-
-typedef struct s_cmd t_cmd;
+typedef struct s_cmd	t_cmd;
 
 typedef struct s_cmd
 {
@@ -69,11 +73,6 @@ typedef struct s_cmd
 	void	(*out_fun)(t_cmd *cmd);
 }	t_cmd;
 
-//One allowed Global variable, it's the hash table
-// that stores the environment variables
-# define ENV_SIZE 2048
-# define LOCAL_ENV_SIZE 256
-
 typedef struct s_env
 {
 	char	*key;
@@ -81,8 +80,8 @@ typedef struct s_env
 	int		*spot;
 }	t_env;
 
-# define ENV_SIZE 2048
-extern t_env	*g_env_table;
+//One allowed Global variable, it's the hash table
+extern t_env			*g_env_table;
 
 // env funs
 u_int64_t	hash(char *str, int envlen);
@@ -94,105 +93,31 @@ void		import_env(char **env);
 int			check_env(char **input);
 int			insert_data(char *line, char *key);
 
-// check_quotations
-char		check_quotations(char to_check, char quotations);
+t_action	*split_actions(char *input, int returnval);
+char		*read_fd(int *fd, bool print);
+char		*ft_strncpy(char *src, int size);
 
-// command_cd
-int			command_cd(const t_inputs *input);
-int			cd_absolute(char **argv);
-int			cd_relative2(char *ext, char *cwdext, char **argv);
-int			cd_relative(char **argv);
-
-// command_echo
-int			command_echo(const t_inputs *input);
-int			get_echo_len(char **argv);
-
-// command_enviroment_utils
-char		*get_data(char *str);
-int			command_env(void);
-int			get_env_len(char *str);
-
-// command_enviroment
-void		import_env(char **env);
-int			insert_data(char *line, char *key);
-int			command_export(t_inputs *input);
-int			unset(t_inputs *input);
-char		*get_key(char *str);
-
-// command_pwd
-int			command_pwd(void);
-
-// ft_joinfree
 char		*ft_joinfree(char *str1, int free1, char *str2, int free2);
 
-// get_options_utils
-int			get_arg_size(char *input, bool include_interpret, int returnval);
-
-// get_options
+int			get_command_id(char *input);
+int			switch_command(char *command, t_args *args, bool *run);
 void		get_options(t_action *action, char *input, int returnval);
-int			get_command(t_action *action, char *input, int returnval);
-int			get_argn(char *input);
-char		*copy_arg(char *input, int returnval);
-
-// hashTable
-u_int64_t	hash(char *str, int envlen);
-t_env		search(char *key);
-void		insert(char *key, char *data);
-
-// main_utils
-void		free_split_input(char **s_input);
-size_t		action_size(t_action *action);
-int			execute_actions2(t_outputs *output, char **stderrs,
-				t_action *action);
-int			execute_actions(t_action *action, bool *run);
-void		switch_relation(t_action *action, t_inputs *input,
-				t_outputs 	*output, bool *run);
-
-// main
-bool		*return_run(bool *run);
-void		handle_sig(int sig);
-int			inti(int argc, char **argv, char **env, bool *run);
-void		run_action(t_action *action, t_inputs *input,
-				t_outputs *output, bool *run);
-
-// read_fd
-char		*read_fd(int *fd, bool print);
-
-// redirections_left
-void		redir_left(char *command, t_outputs *output);
 char		*read_file(char *filename);
-void		insert_doc(char *command, t_outputs *output);
+int			write_file(char *file);
+int			write_file_append(char *file);
+int			redir_left(char *command, int outfd);
+int			insert_doc(char *command, int outfd);
+char		check_quotations(char to_check, char quotations);
 
-// redirections_right
-void		write_to_file(char *input, char *file);
-void		write_to_file_append(char *input, char *file);
-
-// run_cmd_exec
-void		run_cmd_exec2(char *path, char **args, char *command);
-int			run_cmd_exec(char *command, t_inputs *input, t_outputs *output);
-void		next_dir2(bool reset, int *path_pos, const char *path, int *size);
-char		*next_dir(bool reset, const char *path, char *command);
-char		**make_args(char *command, char **args);
-
-// run_executable
-int			run_executable(t_inputs *input, t_outputs *output);
-
-// split_actions_utils
-void		fill_action(t_action *action, char *input, int size, int returnval);
-t_action	*init_next_action(t_action *action);
-char		*ft_strncpy(char *src, int size);
-int			check_pipe_chars(char *input);
-char		*find_relation(char *input);
-
-// split_ations
-int			init_split_actions(t_action	*tempaction);
-t_action	*split_actions(char *input, int returnval);
-int			find_next_seperator(char *input);
-t_action	*redir_reverse(t_action *action, char *input,
-				int *k, int returnval);
-
-// switch_command
-int			switch_command(char *command, t_inputs *input,
-				t_outputs *output, bool *run);
+int			run_executable(const t_args *args);
+int			command_echo(const t_args *args);
+int			command_cd(const t_args *args);
+int			command_pwd(void);
+int			unset(t_args *args);
+int			command_export(t_args *args);
+int			command_env(void);
+int			run_cmd_exec(char *command, t_args *args);
+int			get_arg_size(char *input,
+				bool include_interpret, int returnval);
 
 #endif
